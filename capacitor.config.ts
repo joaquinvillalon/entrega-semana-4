4f2a426356e75aba47d3b77ffcda089bf49305ad
joaquinvillalon <joa.villalon@duocuc.cl>
2024-09-09 23:56:14 -0300

    repo entrega semana 4

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'entrega',
+  webDir: 'www'
+};
+
+export default config;
